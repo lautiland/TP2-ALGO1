de90633bd0866b9b224a714e90a8b4859d7df794
Drope <pmiguel@fi.uba.ar>
2022-05-09 13:57:06 -0300

    verificar arriesgo

--- codigo.py
+++ codigo.py
@@ -1,3 +1,5 @@
+import random
+
 def obtener_color(color):
     colores = {
         "Verde": "\x1b[32m",
@@ -10,21 +12,37 @@ def obtener_color(color):
 def color_de_letra(letra, color):
     return obtener_color(color) + letra.upper() + obtener_color("Defecto")
 
+def obtener_palabras_validas():
+    return["hogar"]
+
+def verificar_arriesgo():
+    while True:
+        arriesgo = input("Arriesgo: ")
+        if len(arriesgo) != 5 or type(arriesgo) != str:
+            arriesgo = input("Ingreso incorrecto.\nArriesgo: ")
+            continue
+        else:
+            return arriesgo
+
 def validacion_letra(arriesgo, solucion):
-    output = ""
+    output = []
     for i in range(len(arriesgo)):
         if arriesgo[i] == solucion[i]:
-            output += color_de_letra(arriesgo[i], "Verde")
+            output.append(color_de_letra(arriesgo[i], "Verde"))
         elif arriesgo[i] in solucion:
-            output += color_de_letra(arriesgo[i], "Amarillo")
+            output.append(color_de_letra(arriesgo[i], "Amarillo"))
         else:
-            output += color_de_letra(arriesgo[i], "GrisOscuro")
-    print(output)
+            output.append(color_de_letra(arriesgo[i], "GrisOscuro"))
     return output
 
 def fiuble():
-    solucion = "hogar"
+    solucion = random.choice(obtener_palabras_validas())
     print("Palabra a adivinar: ? ? ? ? ?")
-    arriesgo = input("Arriesgo: ")
-    print("Palabra a adivinar:", )
-    validacion_letra(arriesgo, solucion)
+    arriesgo = verificar_arriesgo()
+    print("\nPalabra a adivinar:", solucion.upper()[0], solucion.upper()[1], solucion.upper()[2], solucion.upper()[3], solucion.upper()[4])
+    intento = validacion_letra(arriesgo, solucion)
+    print("Arriesgo:", intento[0], intento[1], intento[2], intento[3], intento[4])
+    if arriesgo == solucion:
+        print("Ganaste!")
+    else:
+        print("Perdiste!")
